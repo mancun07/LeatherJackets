20d9ae5c9dafed8e0ffde2b24642c4673d955d6a
Dima <address>
2021-01-16 00:37:38 +0300

    X

--- app/scripts/MobileMenu.js
+++ app/scripts/MobileMenu.js
@@ -1,3 +1,4 @@
+import { transform } from "lodash";
 
 class MobileMenu {
         constructor() {
@@ -5,13 +6,28 @@ class MobileMenu {
             this.sidenav = document.querySelector('.sidenav');
             this.windowWidth = document.body.offsetWidth;
             this.window = document.querySelector('window');
+            this.navbar = document.querySelector('.navbar-wrapper');
             this.events();
         }
 
         events() {
             this.sidenavTrigger.addEventListener('click', () => this.toggleMenu());
-            this.window.addEventListener('resize', () => this.handleWidth());
+            // this.window.addEventListener('resize', () => this.handleWidth());
+            window.addEventListener('scroll', () => this.handleNavbar());
         }
+
+        // handleNavbar() {
+        //     if (pageYOffset > 100) {
+        //             // this.navbar.style.transition = 'translate 1s';  
+        //             this.navbar.style.transform = 'translateY(-150px)';       
+        //         setTimeout(() => {
+        //             this.navbar.style.transform = 'translateY(0px)';  
+        //             this.navbar.style.transition = 'translate 1s';   
+        //             this.navbar.style.position = 'fixed';
+        //         }, 1000)
+                
+        //     }
+        // }
         
         toggleMenu() {
             this.sidenav.classList.toggle('sidenav-is-active');
